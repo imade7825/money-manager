ca39cd43b0db002d04f590949eb24fa8bb504793
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-08-22 17:14:30 +0200

    add accountBalance testing

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,35 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function HomePage() {
-  const { data: transactions } = useSWR("/api/transactions", fetcher);
-  const { data: categories } = useSWR("/api/categories", fetcher);
-  console.log("transaction", transactions);
-  console.log("categories", categories);
-  return <></>;
+  const {
+    data: transactions,
+    error,
+    isLoading,
+    mutate,
+  } = useSWR("/api/transactions", fetcher);
+  //const { data: categories } = useSWR("/api/categories", fetcher);
+
+  if (error) return;
+  if (isLoading) return <p>is Loading..</p>;
+
+  const incomeTotal = transactions
+    .filter((transaction) => transaction.type === "income")
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const expenseTotal = transactions
+    .filter((transaction) => transaction.type === "expense")
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+    const balanceTotal = incomeTotal - expenseTotal;
+    console.log(balanceTotal)
+
+  return (
+    <>
+      <ul>
+        {transactions.map((transaction) => {
+          return <li key={transaction._id}>{transaction.amount}</li>;
+        })}
+      </ul>
+      <p>total Balance: {balanceTotal}</p>
+    </>
+  );
 }
