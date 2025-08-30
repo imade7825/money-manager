175e691d86c904c9494990880e1b94a62e97a99a
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-08-30 13:23:13 +0200

    start implementation backendProtection

--- pages/api/transactions/index.js
+++ pages/api/transactions/index.js
@@ -1,8 +1,12 @@
 import dbConnect from "@/db/connect";
 import Transaction from "@/db/models/Transaction";
 
+
 export default async function handler(request, response) {
   await dbConnect();
+
+  
+
   if (request.method === "GET") {
     try {
       const transactions = await Transaction.find();
@@ -14,26 +18,30 @@ export default async function handler(request, response) {
     }
   }
 
- if (request.method === "POST") {
+  if (request.method === "POST") {
     try {
       const { name, amount, category, type, date } = request.body || {};
 
-     // Validation 
+      // Validation
       if (!name || !amount || !category || !type || !date) {
-        return response.status(400).json({ message: "Bitte alle Felder ausfüllen." });
+        return response
+          .status(400)
+          .json({ message: "Bitte alle Felder ausfüllen." });
       }
 
       const created = await Transaction.create({
         name: String(name).trim(),
         amount: Number(amount),
         category: String(category).trim(),
-        type,             // "income" | "expense"
-        date,             
+        type, // "income" | "expense"
+        date,
       });
 
       return response.status(201).json(created);
     } catch (error) {
-      return response.status(500).json({ message: "Error creating transaction" });
+      return response
+        .status(500)
+        .json({ message: "Error creating transaction" });
     }
   }
   return response.status(405).json({ message: "Method not allowed" });
