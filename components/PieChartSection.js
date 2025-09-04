260617deb3b4322af87035db71dc8d9f47a8155f
Imade <elbahit@gmx.de>
2025-09-04 16:26:04 +0200

    initial branch

--- components/PieChartSection.js
+++ components/PieChartSection.js
@@ -3,4 +3,5 @@ import CategoryPieChart from "./CategoryPieChart";
 export default function PieChartSection({ transactions, open }) {
   if (!open) return null;
   return <div>{<CategoryPieChart transactions={transactions} />}</div>;
+  
 }
